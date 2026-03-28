c230c98298b318accf5c76d84bf404b720059666
mahmoud4malt-web <mahmoud4malt@gmail.com>
2026-03-28 11:12:54 -0700

    Add app constants

---
+++ app/src/main/java/com/emily/prayerpro/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.emily.prayerpro.util
+
+object Constants {
+    const val BASE_URL = "https://api.aladhan.com/v1/"
+    const val PRAYER_DB_NAME = "prayer_db"
+    const val SETTINGS_DATASTORE_NAME = "settings_prefs"
+    
+    // Default location (Mecca)
+    const val DEFAULT_LATITUDE = 21.3891
+    const val DEFAULT_LONGITUDE = 39.8579
+    
+    // Calculation Methods
+    const val METHOD_UMM_AL_QURA = 4
+    const val METHOD_ISNA = 2
+    const val METHOD_MWL = 3
+}
